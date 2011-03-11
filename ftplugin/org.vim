8113249f12010abec04288a0bc6af6f6991e1806
Jan Christoph Ebersbach <jceb@e-jc.de>
2011-03-11 08:46:51 +0100

    create Hyperlink plugin

--- ftplugin/org.vim
+++ ftplugin/org.vim
@@ -2,7 +2,7 @@
 " @Author       : Jan Christoph Ebersbach (jceb@e-jc.de)
 " @License      : GPL (see http://www.gnu.org/licenses/gpl.txt)
 " @Created      : 2010-10-03
-" @Last Modified: Thu 10. Mar 2011 21:49:41 +0100 CET
+" @Last Modified: Fri 11. Mar 2011 07:50:26 +0100 CET
 " @Revision     : 0.1
 " @vi           : ft=vim:tw=80:sw=4:ts=4
 " 
@@ -35,7 +35,7 @@ endif
 
 " general setting plugins that should be loaded and their order
 if ! exists('g:org_plugins') && ! exists('b:org_plugins')
-	let g:org_plugins = ['ShowHide', '|', 'Navigator', 'EditStructure', '|', 'Todo', 'TagsProperties', 'Misc']
+	let g:org_plugins = ['ShowHide', '|', 'Navigator', 'EditStructure', '|', 'Hyperlinks', '|', 'Todo', 'TagsProperties', 'Misc']
 endif
 
 " make sure repeat plugin is load (or not)
@@ -63,45 +63,6 @@ for p in vim.eval("&runtimepath").split(','):
 
 from orgmode import ORGMODE
 EOF
-
-
-" open link
-nnoremap gl :call OrgHyperlinkUTLWrapper('', '')<CR>
-"vnoremap gl :call OrgHyperlinkUTLWrapper('', 'visual')<CR>
-" copy/yank link
-nnoremap gyl :call OrgHyperlinkUTLWrapper('copyLink', '')<CR>
-"vnoremap gyl :call OrgHyperlinkUTLWrapper('copyLink', 'visual')<CR>
-
-function! OrgHyperlinkUTLWrapper(action, visual)
-python << EOF
-import vim, re
-if not int(vim.eval('exists(":Utl")')):
-	echom('Universal Text Linking plugin not installed, unable to proceed.')
-else:
-	action = 'copyLink' if vim.eval('a:action').startswith('copy') else 'openLink'
-	visual = 'visual' if vim.eval('a:visual').startswith('visual') else ''
-
-	cursor = vim.current.window.cursor
-	line = vim.current.buffer[cursor[0] - 1]
-
-	# if the cursor is on the last bracket, it's not recognized as a hyperlink
-	start = line.rfind('[[', 0, cursor[1])
-	end = line.find(']]', cursor[1])
-	match = None
-
-	# extract link
-	if start != -1 and end != -1:
-		uri_match = re.compile('^\[{2}(?P<uri>[^][]*)(\]\[(?P<description>[^][]*))?\]{2}')
-		match = uri_match.match(line[start:end + 2])
-
-	if match:
-		# call UTL with the URI
-		vim.command('Utl %s %s %s' % (action, visual, match.groupdict()['uri']))
-	else:
-		# call UTL and let it decide what to do
-		vim.command('Utl %s %s' % (action, visual))
-EOF
-endfunction
 
 " ******************** Taglist integration ********************
 
---
+++ ftplugin/orgmode/plugins/Hyperlinks.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+from orgmode import echo, echom, echoe, ORGMODE, apply_count, repeat
+from orgmode.menu import Submenu, Separator, ActionEntry
+from orgmode.keybinding import Keybinding, Plug, Command
+from orgmode.heading import Heading, DIRECTION_FORWARD, DIRECTION_BACKWARD
+
+import vim
+import re
+
+class Hyperlinks(object):
+	""" Hyperlinks plugin """
+
+	def __init__(self):
+		""" Initialize plugin """
+		object.__init__(self)
+		# menu entries this plugin should create
+		self.menu = ORGMODE.orgmenu + Submenu('Hyperlinks')
+
+		# key bindings for this plugin
+		# key bindings are also registered through the menu so only additional
+		# bindings should be put in this variable
+		self.keybindings = []
+
+		# commands for this plugin
+		self.commands = []
+
+	def follow(self, action='openLink', visual=''):
+		""" Follow hyperlink. If called on a regular string UTL determines the
+		outcome. Normally a file with that name will be opened.
+
+		:action: "copy" if the link should be copied to clipboard, otherwise the link will be opened
+		:visual: "visual" if Universal Text Linking should be triggered in visual mode
+
+		:returns: URI or None
+		"""
+		if not int(vim.eval('exists(":Utl")')):
+			echom('Universal Text Linking plugin not installed, unable to proceed.')
+			return
+
+		action = 'copyLink' if action and action.startswith('copy') else 'openLink'
+		visual = 'visual' if visual and visual.startswith('visual') else ''
+
+		cursor = vim.current.window.cursor
+		line = vim.current.buffer[cursor[0] - 1]
+
+		# if the cursor is on the last bracket, it's not recognized as a hyperlink
+		start = line.rfind('[[', 0, cursor[1])
+		end = line.find(']]', cursor[1])
+		match = None
+
+		# extract link
+		if start != -1 and end != -1:
+			uri_match = re.compile('^\[{2}(?P<uri>[^][]*)(\]\[(?P<description>[^][]*))?\]{2}')
+			match = uri_match.match(line[start:end + 2])
+
+		if match:
+			# call UTL with the URI
+			uri = match.groupdict()['uri']
+			vim.command('Utl %s %s %s' % (action, visual, uri))
+			return uri
+		else:
+			# call UTL and let it decide what to do
+			vim.command('Utl %s %s' % (action, visual))
+
+	def insert(self, uri=None, description=None):
+		""" Inserts a hyperlink. If no arguments are provided, an interactive
+		query will be started.
+		
+		:uri: The URI that will be opened
+		:description: An optional description that will be displayed instead of the URI
+	
+		:returns: (URI, description)
+	    """
+		try:
+			if uri == None:
+					uri = vim.eval('input("Link: ")')
+
+			if description == None:
+				description = vim.eval('input("Description: ")')
+				if description:
+					description = '][%s' % description
+
+			cursor = vim.current.window.cursor
+			cl = vim.current.buffer[cursor[0] - 1]
+			vim.current.buffer[cursor[0] - 1] = ''.join((cl[:cursor[1]], '[[%s%s]]' % (uri, description), cl[cursor[1]:]))
+		except:
+			import traceback
+			traceback.print_exc()
+			return
+
+	def register(self):
+		"""
+		Registration of plugin. Key bindings and other initialization should be done.
+		"""
+		# an Action menu entry which binds "keybinding" to action ":action"
+		self.commands.append(Command('OrgHyperlinkFollow', ':py ORGMODE.plugins["Hyperlinks"].follow()'))
+		self.keybindings.append(Keybinding('gl', Plug('OrgHyperlinkFollow', self.commands[-1])))
+		self.menu + ActionEntry('&Follow Link', self.keybindings[-1])
+
+		self.commands.append(Command('OrgHyperlinkCopy', ':py ORGMODE.plugins["Hyperlinks"].follow(action="copy")'))
+		self.keybindings.append(Keybinding('gyl', Plug('OrgHyperlinkCopy', self.commands[-1])))
+		self.menu + ActionEntry('&Copy Link', self.keybindings[-1])
+
+		self.commands.append(Command('OrgHyperlinkInsert', ':py ORGMODE.plugins["Hyperlinks"].insert(<args>)', arguments='*'))
+		#self.keybindings.append(Keybinding('gyl', Plug('OrgHyperlinkInsert', self.commands[-1])))
+		#self.menu + ActionEntry('&Copy Link', self.keybindings[-1])
+		self.menu + ActionEntry('&Insert Link', self.commands[-1])
