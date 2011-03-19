05e4fcb7aab5d9bdbc123b0eaaaea7244fb81871
Jan Christoph Ebersbach <jceb@e-jc.de>
2011-03-19 23:46:26 +0100

    implement Hyperlinks plugin

--- ftplugin/orgmode/plugins/Hyperlinks.py
+++ ftplugin/orgmode/plugins/Hyperlinks.py
@@ -25,6 +25,33 @@ class Hyperlinks(object):
 		# commands for this plugin
 		self.commands = []
 
+	uri_match = re.compile('^\[{2}(?P<uri>[^][]*)(\]\[(?P<description>[^][]*))?\]{2}')
+
+	@classmethod
+	def _get_link(cls, cursor=None):
+		"""
+		Get the link the cursor is on and return it's URI and description
+
+		:cursor: None or (Line, Column)
+		:returns: None if no link was found, otherwise {uri:URI, description:DESCRIPTION, line:LINE, start:START, end:END} or uri and description could be None if not set
+		"""
+		cursor = cursor if cursor else vim.current.window.cursor
+		line = vim.current.buffer[cursor[0] - 1]
+
+		# if the cursor is on the last bracket, it's not recognized as a hyperlink
+		start = line.rfind('[[', 0, cursor[1])
+		end = line.find(']]', cursor[1])
+
+		# extract link
+		if start != -1 and end != -1:
+			end += 2
+			match = Hyperlinks.uri_match.match(line[start:end])
+
+			res = {'line':line, 'start':start, 'end':end, 'uri':None, 'description':None}
+			if match:
+				res.update(match.groupdict())
+			return res
+
 	def follow(self, action='openLink', visual=''):
 		""" Follow hyperlink. If called on a regular string UTL determines the
 		outcome. Normally a file with that name will be opened.
@@ -41,24 +68,12 @@ class Hyperlinks(object):
 		action = 'copyLink' if action and action.startswith('copy') else 'openLink'
 		visual = 'visual' if visual and visual.startswith('visual') else ''
 
-		cursor = vim.current.window.cursor
-		line = vim.current.buffer[cursor[0] - 1]
+		link = Hyperlinks._get_link()
 
-		# if the cursor is on the last bracket, it's not recognized as a hyperlink
-		start = line.rfind('[[', 0, cursor[1])
-		end = line.find(']]', cursor[1])
-		match = None
-
-		# extract link
-		if start != -1 and end != -1:
-			uri_match = re.compile('^\[{2}(?P<uri>[^][]*)(\]\[(?P<description>[^][]*))?\]{2}')
-			match = uri_match.match(line[start:end + 2])
-
-		if match:
+		if link and link['uri'] != None:
 			# call UTL with the URI
-			uri = match.groupdict()['uri']
-			vim.command('Utl %s %s %s' % (action, visual, uri))
-			return uri
+			vim.command('Utl %s %s %s' % (action, visual, link['uri']))
+			return link['uri']
 		else:
 			# call UTL and let it decide what to do
 			vim.command('Utl %s %s' % (action, visual))
@@ -72,18 +87,33 @@ class Hyperlinks(object):
 	
 		:returns: (URI, description)
 	    """
+		link = Hyperlinks._get_link()
+		if link:
+			if uri == None and link['uri'] != None:
+				uri = link['uri']
+			if description == None and link['description'] != None:
+				description = link['description']
+
 		try:
 			if uri == None:
-					uri = vim.eval('input("Link: ")')
+				uri = vim.eval('input("Link: ")')
+			elif link:
+				uri = vim.eval('input("Link: ", "%s")' % link['uri'])
 
 			if description == None:
 				description = vim.eval('input("Description: ")')
-				if description:
-					description = '][%s' % description
+			elif link:
+				description = vim.eval('input("Description: ", "%s")' % link['description'])
 
 			cursor = vim.current.window.cursor
 			cl = vim.current.buffer[cursor[0] - 1]
-			vim.current.buffer[cursor[0] - 1] = ''.join((cl[:cursor[1]], '[[%s%s]]' % (uri, description), cl[cursor[1]:]))
+			head = cl[:cursor[1]] if not link else cl[:link['start']]
+			tail = cl[cursor[1]:] if not link else cl[link['end']:]
+
+			separator = ''
+			if description:
+				separator = ']['
+			vim.current.buffer[cursor[0] - 1] = ''.join((head, '[[%s%s%s]]' % (uri, separator, description), tail))
 		except:
 			import traceback
 			traceback.print_exc()
@@ -102,7 +132,6 @@ class Hyperlinks(object):
 		self.keybindings.append(Keybinding('gyl', Plug('OrgHyperlinkCopy', self.commands[-1])))
 		self.menu + ActionEntry('&Copy Link', self.keybindings[-1])
 
-		self.commands.append(Command('OrgHyperlinkInsert', ':py ORGMODE.plugins["Hyperlinks"].insert(<args>)', arguments='*'))
-		#self.keybindings.append(Keybinding('gyl', Plug('OrgHyperlinkInsert', self.commands[-1])))
-		#self.menu + ActionEntry('&Copy Link', self.keybindings[-1])
-		self.menu + ActionEntry('&Insert Link', self.commands[-1])
+		self.commands.append(Command('OrgHyperlinkInsert', ':py ORGMODE.plugins["Hyperlinks"].insert(<f-args>)', arguments='*'))
+		self.keybindings.append(Keybinding('gil', Plug('OrgHyperlinkInsert', self.commands[-1])))
+		self.menu + ActionEntry('&Insert Link', self.keybindings[-1])
