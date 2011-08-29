73489058f31d9d899e3101048ada89376aee5e9d
Stefan Otte <stefan.otte@gmail.com>
2011-08-29 22:30:44 +0200

    basic functionality for orddates and some unittests.

--- ftplugin/orgmode/liborgmode/orgdate.py
+++ ftplugin/orgmode/liborgmode/orgdate.py
@@ -14,25 +14,63 @@
     They can be active or inactive
 """
 
-
-def get_orgdate():
-    """
-    parse the given text and return an OrgDate if text contains a string
-    representation of an OrgDate.
-    """
-    pass
+import datetime
+import re
 
 
-class OrgDate(object):
-    """
-    OrgDate represents a normal date like '2011-08-29 Mon'.
+_DATE_REGEX = re.compile(r"<(\d\d\d\d)-(\d\d)-(\d\d) [A-Z]\w\w>")
 
-    OrgDates can be active or inactive
 
-    """
-    def __init__(self, arg):
-        super(OrgDate, self).__init__()
-        self.arg = arg
+def get_orgdate(text):
+	"""
+	parse the given text and return an OrgDate if text contains a string
+	representation of an OrgDate.
+	"""
+	result = _DATE_REGEX.search(text)
+	if result:
+		matches = [int(m) for m in result.groups()]
+		try:
+			return OrgDate(True, matches[0], matches[1], matches[2])
+		except Exception:
+			return None
+	else:
+		return None
+
+
+class OrgDate(datetime.date):
+	"""
+	OrgDate represents a normal date like '2011-08-29 Mon'.
+
+	OrgDates can be active or inactive.
+
+	NOTE: date is immutable. Thats why there needs to be __new__().
+	See: http://docs.python.org/reference/datamodel.html#object.__new__
+	"""
+	def __init__(self, active, year, month, day):
+		self.active = active
+		pass
+
+	def __new__(cls, active, year, month, day):
+		return datetime.date.__new__(cls, year, month, day)
+
+
+	def __str__(self):
+		if self.active:
+			return self.strftime(u'<%Y-%m-%d %a>')
+		else:
+			return self.strftime(u'[%Y-%m-%d %a]')
+
+
+class OrgDateRange(object):
+	"""
+	OrgDate represents a range of dates like
+	* <2011-08-29 Mon>--<2011-08-30 Tue>
+	* <2011-08-29 Mon 10:12>--<2011-08-29 Mon 14:30>
+	* [2011-08-29 Mon]--[2011-08-30 Tue]
+	* [2011-08-29 Mon 12:00]--[2011-08-30 Tue 14:00]
+	"""
+	def __init__(self):
+		super(OrgDateRange, self).__init__()
 
 
 # vim: set noexpandtab:
--- tests/test_orgdate.py
+++ tests/test_orgdate.py
@@ -4,10 +4,11 @@ import unittest
 import sys
 sys.path.append(u'../ftplugin')
 
-#from datetime import date
-#from datetime import datetime
+from datetime import date
+from datetime import datetime
 
-#from orgmode.plugins.Date import Date
+from orgmode.liborgmode.orgdate import get_orgdate
+from orgmode.liborgmode.orgdate import OrgDate
 
 
 class OrgDateTestCase(unittest.TestCase):
@@ -16,11 +17,72 @@ class OrgDateTestCase(unittest.TestCase):
 	"""
 
 	def setUp(self):
-		pass
+		self.date = date(2011, 8, 29)
+		self.year = 2011
+		self.month = 8
+		self.day = 29
 
-	def test(self):
-		pass
+		self.text = '<2011-08-29 Mon>'
+		self.textinactive = '[2011-08-29 Mon]'
 
+	def test_OrgDate_ctor(self):
+		"""OrdDate should be created."""
+		today = date.today()
+		od = OrgDate(True, today.year, today.month, today.day)
+		self.assertTrue(isinstance(od, OrgDate))
+		self.assertTrue(od.active)
+
+		od = OrgDate(False, today.year, today.month, today.day)
+		self.assertTrue(isinstance(od, OrgDate))
+		self.assertFalse(od.active)
+
+	def test_OrdDate_str(self):
+		"""Representation of OrgDates"""
+		od = OrgDate(True, self.year, self.month, self.day)
+		self.assertEqual(self.text, str(od))
+
+		od = OrgDate(False, self.year, self.month, self.day)
+		self.assertEqual(self.textinactive, str(od))
+
+
+	def test_get_orgdate_parsing(self):
+		"""
+		get_orgdate should recognice all orgdates in a given text
+		"""
+		result = get_orgdate(self.text)
+		self.assertIsNotNone(result)
+		self.assertTrue(isinstance(result, OrgDate))
+		self.assertTrue(isinstance(get_orgdate("<2011-08-30 Tue>"), OrgDate))
+		self.assertEqual(get_orgdate("<2011-08-30 Tue>").year, 2011)
+		self.assertEqual(get_orgdate("<2011-08-30 Tue>").month, 8)
+		self.assertEqual(get_orgdate("<2011-08-30 Tue>").day, 30)
+
+		datestr = "This date <2011-08-30 Tue> is embedded"
+		self.assertTrue(isinstance(get_orgdate(datestr), OrgDate))
+
+
+	def test_get_orgdate_parsing_with_invalid_input(self):
+		self.assertIsNone(get_orgdate("NONSENSE"))
+		self.assertIsNone(get_orgdate("No D<2011- Date 08-29 Mon>"))
+		self.assertIsNone(get_orgdate("2011-08-r9 Mon]"))
+		self.assertIsNone(get_orgdate("<2011-08-29 Mon"))
+		self.assertIsNone(get_orgdate("<2011-08-29 Mon]"))
+		self.assertIsNone(get_orgdate("2011-08-29 Mon"))
+		self.assertIsNone(get_orgdate("2011-08-29"))
+		self.assertIsNone(get_orgdate("2011-08-29 mon"))
+		self.assertIsNone(get_orgdate("<2011-08-29 mon>"))
+
+		self.assertIsNone(get_orgdate("wrong date embedded <2011-08-29 mon>"))
+		self.assertIsNone(get_orgdate("wrong date <2011-08-29 mon>embedded "))
+
+
+	def test_get_orgdate_parsing_with_invalid_dates(self):
+		"""Something like <2011-14-29 Mon> should not be parsed"""
+		datestr = "<2011-14-30 Tue>"
+		self.assertIsNone(get_orgdate(datestr))
+
+		datestr = "<2012-03-40 Tue>"
+		self.assertIsNone(get_orgdate(datestr))
 
 def suite():
 	return unittest.TestLoader().loadTestsFromTestCase(OrgDateTestCase)
