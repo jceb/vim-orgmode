92965600d044e75e20ef08436146273bc1c8d8f9
Stefan Otte <stefan.otte@gmail.com>
2011-09-02 23:09:51 +0200

    Adjust agenda to new filtering with agendafilter.

--- ftplugin/orgmode/liborgmode/agenda.py
+++ ftplugin/orgmode/liborgmode/agenda.py
@@ -11,59 +11,34 @@
 	* sorting
 """
 
+from orgmode.liborgmode.agendafilter import *
+
+
 class AgendaManager(object):
 	"""Simple parsing of Documents to create an agenda."""
 	def __init__(self):
 		super(AgendaManager, self).__init__()
 		self.agenda = []
 
-	def get_agenda_todo(self, document):
+	def get_todo(self, document):
 		"""
 		Get the todo agenda.
 		"""
 		# empty agenda
 		self.agenda[:] = []
-		# setup todo filter
-		self.all_criteria = [self.criteria_todo]
 		# filter and return headings
-		return filter(self.meets_criteria, document.all_headings())
+		filtered = filter_items(document.all_headings(), [contains_active_todo])
+		return filtered
 
-	def get_agenda(self, document, criteria=None):
-		""""
-		Get an agenda.
+	def get_next_week(self, document):
 		"""
-		self.document = document
-		self.active_todo_states = document.get_todo_states()[0]
-
+		Get the agenda for next week.
+		"""
 		# empty agenda
 		self.agenda[:] = []
+		# filter and return headings
+		filtered = filter_items(document.all_headings(), [is_within_week])
+		return filtered
 
-		# setup filter
-		self.all_criteria = [
-				self.criteria_active_todo,
-				self.criteria_active_date
-				]
-		# filter headings
-		return filter(self.meets_criteria, document.all_headings())
-
-
-	def meets_criteria(self, heading):
-		for fn in self.all_criteria:
-			if fn(heading):
-				return True
-		return False
-
-	def criteria_active_todo(self, heading):
-		"""
-		Return True if heading contains an active TODO.
-		"""
-		TODO, DONE = self.active_todo_states
-		return heading.todo == "TODO"
-
-	def criteria_active_date(self, heading):
-		"""
-		Return True if heading contains an active date.
-		"""
-		return not heading.active_date is None
 
 # vim: set noexpandtab:
