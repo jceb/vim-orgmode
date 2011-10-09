jceb/vim-orgmode

commit ea1e84e25145b64e30ade0eae8ae340acfcf7da1

optimize fold caching even further

indent/org.vim
@@ -44,7 +44,6 @@ from orgmode import fold_orgmode
 fold_orgmode(allow_dirty=True)
 EOF
 	else
-		"unlet! b:org_folding_cache
 python << EOF
 from orgmode import fold_orgmode
 fold_orgmode()
@@ -55,7 +54,8 @@ EOF
 		let l:tmp = b:fold_expr
 		unlet b:fold_expr
 		if exists('b:org_folding_cache')
-			if len(b:org_folding_cache) > 3
+			if ! has_key(b:org_folding_cache, v:lnum) &&
+						\ len(b:org_folding_cache) == 3
 				let b:org_folding_cache = {}
 			endif
 			if ! has_key(b:org_folding_cache, v:lnum)
@@ -90,7 +90,6 @@ from orgmode import fold_text
 fold_text(allow_dirty=True)
 EOF
 	else
-		"unlet! b:org_folding_cache
 python << EOF
 from orgmode import fold_text
 fold_text()
@@ -100,14 +99,10 @@ EOF
 	if exists('b:foldtext')
 		let l:tmp = b:foldtext
 		unlet b:foldtext
-		if exists('b:org_folding_cache')
-			if len(b:org_folding_cache) > 3
-				let b:org_folding_cache = {}
-			endif
-			if has_key(b:org_folding_cache, v:lnum) &&
+		if exists('b:org_folding_cache') &&
+					\ has_key(b:org_folding_cache, v:lnum) &&
 					\ len(b:org_folding_cache[v:lnum]) == 1
-				let add(b:org_folding_cache[v:lnum], l:tmp)
-			endif
+			call add(b:org_folding_cache[v:lnum], l:tmp)
 		endif
 		return l:tmp
 	endif

commit 9d38dce8d19e20edc9917fc5e97f36d64672a824

fix regression in fold text introduced by commit ea1e84e25145b64e30ade0eae8ae340acfcf7da1 (closes issue #103)

indent/org.vim
@@ -53,12 +53,11 @@ EOF
 	if exists('b:fold_expr')
 		let l:tmp = b:fold_expr
 		unlet b:fold_expr
-		if exists('b:org_folding_cache')
-			if ! has_key(b:org_folding_cache, v:lnum) &&
-						\ len(b:org_folding_cache) == 3
-				let b:org_folding_cache = {}
-			endif
+		if mode() == 'i'
 			if ! has_key(b:org_folding_cache, v:lnum)
+				if len(b:org_folding_cache) > 3
+					let b:org_folding_cache = {}
+				endif
 				let b:org_folding_cache[v:lnum] = [l:tmp]
 			endif
 		endif
@@ -99,10 +98,12 @@ EOF
 	if exists('b:foldtext')
 		let l:tmp = b:foldtext
 		unlet b:foldtext
-		if exists('b:org_folding_cache') &&
-					\ has_key(b:org_folding_cache, v:lnum) &&
-					\ len(b:org_folding_cache[v:lnum]) == 1
-			call add(b:org_folding_cache[v:lnum], l:tmp)
+		if mode() == 'i' && has_key(b:org_folding_cache, v:lnum) &&
+			if len(b:org_folding_cache[v:lnum]) == 1
+				call add(b:org_folding_cache[v:lnum], l:tmp)
+			else
+				let b:org_folding_cache[v:lnum][2] = l:tmp
+			endif
 		endif
 		return l:tmp
 	endif