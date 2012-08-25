2e52390f84ac46a04bf858ef399452200a4f3eb5
Stefan Otte <stefan.otte@gmail.com>
2012-08-25 21:53:15 +0200

    pep8ify exceptions.py and settings.py.

--- ftplugin/orgmode/exceptions.py
+++ ftplugin/orgmode/exceptions.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
+
 class PluginError(Exception):
 	def __init__(self, message):
 		Exception.__init__(self, message)
+
 
 class BufferNotFound(Exception):
 	def __init__(self, message):
 		Exception.__init__(self, message)
 
+
 class BufferNotInSync(Exception):
 	def __init__(self, message):
 		Exception.__init__(self, message)
+
 
 class HeadingDomError(Exception):
 	def __init__(self, message):
--- ftplugin/orgmode/settings.py
+++ ftplugin/orgmode/settings.py
@@ -14,6 +14,7 @@ VARIABLE_LEADER = {SCOPE_GLOBAL: u'g', SCOPE_BUFFER: u'b'}
 
 u""" Evaluate and store settings """
 
+
 def get(setting, default=None, scope=SCOPE_ALL):
 	u""" Evaluate setting in scope of the current buffer,
 	globally and also from the contents of the current buffer
@@ -28,18 +29,21 @@ def get(setting, default=None, scope=SCOPE_ALL):
 	"""
 	# TODO first read setting from org file which take precedence over vim
 	# variable settings
-	if scope & SCOPE_ALL | SCOPE_BUFFER and int(vim.eval((u'exists("b:%s")' % setting).encode(u'utf-8'))):
+	if (scope & SCOPE_ALL | SCOPE_BUFFER) and \
+			int(vim.eval((u'exists("b:%s")' % setting).encode(u'utf-8'))):
 		res = vim.eval((u"b:%s" % setting).encode(u'utf-8'))
 		if type(res) in (unicode, str):
 			return res.decode(u'utf-8')
 		return res
 
-	elif scope & SCOPE_ALL | SCOPE_GLOBAL and int(vim.eval((u'exists("g:%s")' % setting).encode(u'utf-8'))):
+	elif (scope & SCOPE_ALL | SCOPE_GLOBAL) and \
+			int(vim.eval((u'exists("g:%s")' % setting).encode(u'utf-8'))):
 		res = vim.eval((u"g:%s" % setting).encode(u'utf-8'))
 		if type(res) in (unicode, str):
 			return res.decode(u'utf-8')
 		return res
 	return default
+
 
 def set(setting, value, scope=SCOPE_GLOBAL, overwrite=False):
 	u""" Store setting in the definied scope
@@ -48,15 +52,19 @@ def set(setting, value, scope=SCOPE_GLOBAL, overwrite=False):
 	different value is received by vim.eval, e.g. a list or dict, no conversion
 	is done.
 
-	:setting: name of the setting
-	:value: the actual value, repr is called on the value to create a string representation
-	:scope: the scope o the setting/variable
+	:setting:   name of the setting
+	:value:     the actual value, repr is called on the value to create a string
+	            representation
+	:scope:     the scope o the setting/variable
 	:overwrite: overwrite existing settings (probably user definied settings)
 
 	:returns: the new value in case of overwrite==False the current value
 	"""
-	if not overwrite and int(vim.eval((u'exists("%s:%s")' % (VARIABLE_LEADER[scope], setting)).encode(u'utf-8'))):
-		res = vim.eval((u'%s:%s' % (VARIABLE_LEADER[scope], setting)).encode(u'utf-8'))
+	if (not overwrite) and (
+			int(vim.eval((u'exists("%s:%s")' % \
+			(VARIABLE_LEADER[scope], setting)).encode(u'utf-8')))):
+		res = vim.eval(
+				(u'%s:%s' % (VARIABLE_LEADER[scope], setting)).encode(u'utf-8'))
 		if type(res) in (unicode, str):
 			return res.decode(u'utf-8')
 		return res
@@ -65,18 +73,21 @@ def set(setting, value, scope=SCOPE_GLOBAL, overwrite=False):
 		# strip leading u of unicode string representations
 		v = v[1:]
 
-	vim.command((u'let %s:%s = %s' % (VARIABLE_LEADER[scope], setting, v)).encode(u'utf-8'))
+	cmd = u'let %s:%s = %s' % (VARIABLE_LEADER[scope], setting, v)
+	vim.command(cmd.encode(u'utf-8'))
 	return value
+
 
 def unset(setting, scope=SCOPE_GLOBAL):
 	u""" Unset setting int the definied scope
 	:setting: name of the setting
-	:scope: the scope o the setting/variable
+	:scope:   the scope o the setting/variable
 
 	:returns: last value of setting
 	"""
 	value = get(setting, scope=scope)
-	vim.command((u'unlet! %s:%s' % (VARIABLE_LEADER[scope], setting)).encode(u'utf-8'))
+	cmd = u'unlet! %s:%s' % (VARIABLE_LEADER[scope], setting)
+	vim.command(cmd.encode(u'utf-8'))
 	return value
 
 
