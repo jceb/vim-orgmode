9b8451b15b561ca202bce00c78323e91a22fa55c
Powen Tan <powentan0330@gmail.com>
2014-04-03 18:13:47 +0800

    Make indention consist with vim's "noexpandtab" setting

--- tests/test_plugin_edit_checkbox.py
+++ tests/test_plugin_edit_checkbox.py
@@ -12,20 +12,20 @@ PLUGIN_NAME = u'EditCheckbox'
 
 
 def set_vim_buffer(buf=None, cursor=(2, 0), bufnr=0):
-    if buf is None:
-        buf = []
-    vim.current.buffer[:] = buf
-    vim.current.window.cursor = cursor
-    vim.current.buffer.number = bufnr
+	if buf is None:
+		buf = []
+	vim.current.buffer[:] = buf
+	vim.current.window.cursor = cursor
+	vim.current.buffer.number = bufnr
 
 
 class EditCheckboxTestCase(unittest.TestCase):
-    def setUp(self):
-        if PLUGIN_NAME not in ORGMODE.plugins:
-            ORGMODE.register_plugin(PLUGIN_NAME)
-        self.editcheckbox = ORGMODE.plugins[PLUGIN_NAME]
+	def setUp(self):
+		if PLUGIN_NAME not in ORGMODE.plugins:
+			ORGMODE.register_plugin(PLUGIN_NAME)
+		self.editcheckbox = ORGMODE.plugins[PLUGIN_NAME]
 
-        self.c1 = u"""
+		self.c1 = u"""
 * heading1 [%]
  - [ ] checkbox1 [/]
   - [ ] checkbox2
@@ -35,7 +35,7 @@ class EditCheckboxTestCase(unittest.TestCase):
   - [ ] checkbox6
 """.split(u'\n')
 
-        self.c2 = u"""
+		self.c2 = u"""
 * a checkbox list [%]
 - checkbox [0%]
  - [ ] test1
@@ -43,36 +43,38 @@ class EditCheckboxTestCase(unittest.TestCase):
  - [ ] test3
 """.split(u'\n')
 
-    def test_toggle(self):
-        bufnr = 10
-        # test on self.c1
-        set_vim_buffer(buf=self.c1, cursor=(6, 0), bufnr=bufnr)
-        # update_checkboxes_status
-        self.editcheckbox.update_checkboxes_status()
-        self.assertEqual(vim.current.buffer[1], "* heading1 [0%]")
-        # toggle
-        self.editcheckbox.toggle()
-        self.assertEqual(vim.current.buffer[5], "    - [X] checkbox4")
-        vim.current.window.cursor = (7, 0)
-        # new_checkbox
-        self.editcheckbox.new_checkbox(below=True)
-        self.assertEqual(vim.current.buffer[8], ' - [ ] ')
-        self.editcheckbox.update_checkboxes_status()
+	def test_toggle(self):
+		bufnr = 10
+		# test on self.c1
+		set_vim_buffer(buf=self.c1, cursor=(6, 0), bufnr=bufnr)
+		# update_checkboxes_status
+		self.editcheckbox.update_checkboxes_status()
+		self.assertEqual(vim.current.buffer[1], "* heading1 [0%]")
+		# toggle
+		self.editcheckbox.toggle()
+		self.assertEqual(vim.current.buffer[5], "    - [X] checkbox4")
+		vim.current.window.cursor = (7, 0)
+		# new_checkbox
+		self.editcheckbox.new_checkbox(below=True)
+		self.assertEqual(vim.current.buffer[8], ' - [ ] ')
+		self.editcheckbox.update_checkboxes_status()
 
-    def test_no_status_checkbox(self):
-        bufnr = 11
-        # test on self.c2
-        set_vim_buffer(buf=self.c2, bufnr=bufnr)
-        self.assertEqual(vim.current.buffer[2], "- checkbox [0%]")
-        # toggle
-        vim.current.window.cursor = (4, 0)
-        self.editcheckbox.toggle()
-        self.assertEqual(vim.current.buffer[3], " - [X] test1")
+	def test_no_status_checkbox(self):
+		bufnr = 11
+		# test on self.c2
+		set_vim_buffer(buf=self.c2, bufnr=bufnr)
+		self.assertEqual(vim.current.buffer[2], "- checkbox [0%]")
+		# toggle
+		vim.current.window.cursor = (4, 0)
+		self.editcheckbox.toggle()
+		self.assertEqual(vim.current.buffer[3], " - [X] test1")
 
-        self.editcheckbox.update_checkboxes_status()
-        # see if the no status checkbox update its status
-        self.assertEqual(vim.current.buffer[2], "- checkbox [33%]")
+		self.editcheckbox.update_checkboxes_status()
+		# see if the no status checkbox update its status
+		self.assertEqual(vim.current.buffer[2], "- checkbox [33%]")
 
 
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(EditCheckboxTestCase)
+	return unittest.TestLoader().loadTestsFromTestCase(EditCheckboxTestCase)
+
+# vim: set noexpandtab:
